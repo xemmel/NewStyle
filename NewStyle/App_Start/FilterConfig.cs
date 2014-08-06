f7e2623441819932f1bb6b5cceeba573f67dd3de
xemmel <lacour@gmail.com>
2014-08-06 11:01:14 +0200

    Init

---
+++ NewStyle/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NewStyle
+{
+  public class FilterConfig
+  {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+    {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
